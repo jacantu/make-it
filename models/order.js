7fbdd7c2540ee057c85e29f5e6ddc8bc321b8e75
Adrian Cantu <ja.cantu@my.codergv.org>
2016-11-29 09:51:47 -0600

    add css

--- models/order.js
+++ models/order.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var schema = new Schema({
 
-    user: {type: Schema.Types.ObjectId, ref: 'User'},/** this user field will hold an id refering to User model */
+    user: {type: Schema.Types.ObjectId, ref: 'User'},/** this user field will hold an id referring to User model */
     cart: {type: Object, required: true},
     address: {type: String, required: true},
     name: {type: String, required: true},
